d17ef671f7ed43e7cc7b56575b0b39848a36cb4a
calista salsabila <calistasalsabila313@gmail.com>
2025-12-01 12:09:21 +0700

    Add last_pbo as normal folder

---
+++ last_pbo/pomodoro-app/src/main/java/com/pomodoro/model/TimerModel.java
@@ -0,0 +1,40 @@
+package com.pomodoro.model;
+
+import javafx.beans.property.*;
+
+public class TimerModel {
+    // Default awal (bisa diubah nanti)
+    private int currentStartTime = 25 * 60; 
+    
+    private IntegerProperty timeSeconds = new SimpleIntegerProperty(currentStartTime);
+    private BooleanProperty isRunning = new SimpleBooleanProperty(false);
+
+    public IntegerProperty timeSecondsProperty() { return timeSeconds; }
+    public BooleanProperty isRunningProperty() { return isRunning; }
+
+    public int getTimeSeconds() { return timeSeconds.get(); }
+    public boolean isRunning() { return isRunning.get(); }
+
+    public void setRunning(boolean running) { this.isRunning.set(running); }
+
+    //pilih waktu
+    public void setStartTime(int seconds) {
+        this.currentStartTime = seconds;
+        this.timeSeconds.set(seconds); // Update waktu saat ini juga
+    }
+
+    public void decrementTime() {
+        if (timeSeconds.get() > 0) {
+            timeSeconds.set(timeSeconds.get() - 1);
+        }
+    }
+
+    public void reset() {
+        timeSeconds.set(currentStartTime); // Reset kembali ke waktu yang dipilih user
+        setRunning(false);
+    }
+
+    public enum SessionMode {
+        WORK, BREAK
+    }
+}
